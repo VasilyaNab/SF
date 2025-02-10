74fd3ddfa2a829d1b943f0b844977ba15c735ab5
Aska <146079015+VasilyaNab@users.noreply.github.com>
2025-02-10 21:19:33 +0400

    Delete newsModul21/NewsPaper directory

--- newsModul21/NewsPaper/news/scheduler.py
+++
@@ -1,3 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-
-subscribe_scheduler = BackgroundScheduler()
--- newsModul21/NewsPaper/news/tasks.py
+++
@@ -1,4 +0,0 @@
-from django.core.mail import mail_managers
-
-def send_mails():
-    print('Hello from background task!')
